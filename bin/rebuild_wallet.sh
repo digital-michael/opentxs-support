fe17e838b391f794c47c2f9220436323812f51c9
michael <michael@users.noreply.github.com>
2019-03-15 12:11:18 -0500

    updated rebuild_wallet to more closely mirror the other rebuild commants; updated setup documentation to include Qt5-devel instructions

--- bin/rebuild_wallet.sh
+++ bin/rebuild_wallet.sh
@@ -1,12 +1,26 @@
 #!/usr/bin/bash
 MOD="${1}"
+set -e 
+
 # START CUSTOMIZATION ZONE - for your system
 
-SOURCE_DIR="/storage/workspace/Employers/StashCrypto/rune/runewallet${MOD}"
-FELGO_ROOT="/opt/Felgo/Felgo"
+#SOURCE_DIR="/storage/workspace/Employers/StashCrypto/rune/runewallet${MOD}"
+#FELGO_ROOT="/opt/Felgo/Felgo"
 
 # END CUSTOMIZATION ZONE
 
+# load configuration realitive to this script
+SCRIPT=$(readlink -f $0)
+SCRIPTPATH=`dirname $SCRIPT`
+source "${SCRIPTPATH}/build"
+
+PRJ=runewallet
+SOURCE_DIR="${SOURCES_RUNE}/${PRJ}${MOD}"
+
+if [[ ! -d "${SOURCE_DIR}" ]] ; then
+        cd "${SOURCES_RUNE}"
+	git clone git@github.com:digital-michael/${PRJ}.git ${PRJ}${MOD}
+fi
 
 cd "${SOURCE_DIR}"
 rm -rf "${SOURCE_DIR}/build"
@@ -15,7 +29,7 @@ cd "${SOURCE_DIR}/build"
 
 "${FELGO_ROOT}/gcc_64/bin/qmake" -project .. 
 
-/opt/Felgo/Felgo/gcc_64/bin/qmake \
+${FELGO_ROOT}/gcc_64/bin/qmake \
 INCLUDEPATH+="${FELGO_ROOT}/gcc_64/include/QtWidgets" \
 INCLUDEPATH+="${FELGO_ROOT}/gcc_64/include/Felgo"  \
 INCLUDEPATH+="${FELGO_ROOT}/gcc_64/include/QtQml" \
@@ -32,3 +46,4 @@ LIBS+=-lopentxs
 
 make -j`nproc`
 
+echo Finished work in ${SOURCES_RUNE}
