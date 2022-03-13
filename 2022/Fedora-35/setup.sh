2504db088cce1dc08a99cd52e13fb74c3b2951f4
digital-michael <michaelbiggerstaff7@gmail.com>
2022-03-13 13:50:58 -0500

    Added more notes in setup.sh to get on the correct path quicker

--- 2022/Fedora-35/setup.sh
+++ 2022/Fedora-35/setup.sh
@@ -9,5 +9,16 @@ cd ~
 git clone git@github.com:matterfi/otscripts.git
 cd otscripts
 ./init
-# reference local setup for fedora KDE 35 in .env-fedora-kde-35
+echo reference local setup for fedora KDE 35 in .env-fedora-kde-35
+
+# set now so we can run
+OT_SCRIPT_PARENT=${HOME}
+PATH=${OT_SCRIPT_PARENT}/otscripts/:${PATH}
+
+# add to ~/localrc to enable running later
+echo OT_SCRIPT_PARENT=${HOME} >> ${HOME}/localrc
+echo PATH=${OT_SCRIPT_PARENT}/otscripts/:${PATH} >> ${HOME/localrc
+
+echo execute: rebuild_opentxs full gcc
+echo execute: rebuild_metier gcc
 
